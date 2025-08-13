0b5616f9eb4fc643ac6752cdf35378b68892d2de
Junhyeok Oh <odocodani@gmail.com>
2025-08-13 19:57:13 +0900

    Remove trace feature and add some regions

--- Breakout/Breakout.cpp
+++ Breakout/Breakout.cpp
@@ -4,8 +4,7 @@
 #include <chrono>
 #include <cmath>
 
-#undef ENABLE_BALL_TRACE
-
+#pragma region types
 struct Position
 {
 	float x;
@@ -23,7 +22,8 @@ struct Size
 	int width;
 	int height;
 };
-
+#pragma endregion
+#pragma region functions
 static void DrawCircle(SDL_Renderer* renderer, Position center, int radius)
 {
 	const int diameter = (radius * 2);
@@ -56,16 +56,29 @@ static void DrawCircle(SDL_Renderer* renderer, Position center, int radius)
 		}
 	}
 }
+#pragma endregion
 
 int main(int argc, char** args)
 {
+#pragma region decl
+	// window
 	const Size windowSize = { 800, 600 };
+	SDL_Window* window = nullptr;
 
+	// renderer
+	SDL_Renderer* renderer = nullptr;
+
+	// for game loop
+	SDL_Event e{};
+	bool running = true;
+
+	// paddle
 	Size paddleSize = { 200, 10 };
 	Position paddlePos = { (windowSize.width - paddleSize.width) / 2.0f, (float)((windowSize.height - 40) - paddleSize.height)};
 	int paddleVelocity = 15;
 	SDL_Rect paddleShape{};
 
+	// ball
 	int ballRadius = 10;
 	Position ballCenter = { paddlePos.x + paddleSize.width / 2, paddlePos.y - ballRadius * 2};
 	float ballVelocity = 7;
@@ -74,40 +87,44 @@ int main(int argc, char** args)
 	std::chrono::time_point<clock> ballOutTime;
 	bool ballWaiting = false;
 	auto respawnDelay = std::chrono::milliseconds(2000);
+	
+	// for paddle-ball hit processing
 	float hitFactor;
 	float bounceAngleAcute;
 	float bounceAngle;
 	float bounceAngleBoundary = (float)M_PI / 18.0f;
+#pragma endregion
 
-#ifdef ENABLE_BALL_TRACE
-	std::vector<Position> trace;
-#endif
-
+#pragma region init
+	// init SDL
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
 	{
 		std::cout << "Error initializing SDL: " << SDL_GetError() << std::endl;
 		return 1;
 	}
 
-	SDL_Window* window = SDL_CreateWindow("Breakout", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowSize.width, windowSize.height, SDL_WINDOW_SHOWN);
+	// create window and renderer
+	window = SDL_CreateWindow("Breakout", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowSize.width, windowSize.height, SDL_WINDOW_SHOWN);
 	if (!window)
 	{
 		std::cout << "Error creating window: " << SDL_GetError() << std::endl;
 		return 1;
 	}
 
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (!renderer)
 	{
 		std::cout << "Error creating renderer: " << SDL_GetError() << std::endl;
 		return 1;
 	}
+#pragma endregion
 
-	SDL_Event e{};
-	bool running = true;
+#pragma region loop
 
+	// game loop
 	while (running)
 	{
+#pragma region input
 		while (SDL_PollEvent(&e))
 		{
 			if (e.type == SDL_QUIT)
@@ -117,7 +134,8 @@ int main(int argc, char** args)
 		}
 
 		const unsigned char* keyboardState = SDL_GetKeyboardState(nullptr);
-		
+#pragma endregion
+#pragma region update
 		if (keyboardState[SDL_SCANCODE_LEFT])
 		{
 			paddlePos.x -= paddleVelocity;
@@ -153,8 +171,7 @@ int main(int argc, char** args)
 			ballMotion.dx *= -1;
 		}
 
-		// out
-		// TODO: improve behavior when out
+		// ball out
 		if (!ballWaiting && ballCenter.y > windowSize.height + ballRadius) 
 		{
 			ballMotion = { 0, 0 };
@@ -166,9 +183,6 @@ int main(int argc, char** args)
 		{
 			if (clock::now() - ballOutTime >= respawnDelay) 
 			{
-#ifdef ENABLE_BALL_TRACE
-				trace.clear();
-#endif
 				ballCenter = { paddlePos.x + paddleSize.width / 2, paddlePos.y - ballRadius * 2 };
 				ballMotion = { -ballVelocity, -ballVelocity };
 
@@ -176,18 +190,15 @@ int main(int argc, char** args)
 			}
 		}
 
-
-		// ball-paddle collision check
-		// TODO: Check collision on all sides of the paddle
-		if (
-			(ballCenter.y + ballRadius >= paddlePos.y) && 
+		// collision detection between ball and paddle
+		if ((ballCenter.y + ballRadius >= paddlePos.y) && 
 			(ballCenter.y < paddlePos.y) && 
 			(paddlePos.x - ballRadius <= ballCenter.x) && 
 			(ballCenter.x <= paddlePos.x + paddleSize.width + ballRadius))
 		{
 			hitFactor = (ballCenter.x - (paddlePos.x + paddleSize.width / 2.0f)) / (paddleSize.width / 2.0f + ballRadius);
-			
 			bounceAngleAcute = bounceAngleBoundary + ((float)M_PI / 2.0f - bounceAngleBoundary) * (1.0f - std::abs(hitFactor));
+
 			if (hitFactor < 0)
 			{
 				bounceAngle = (float)M_PI - bounceAngleAcute;
@@ -206,7 +217,8 @@ int main(int argc, char** args)
 
 		ballCenter.x += ballMotion.dx;
 		ballCenter.y += ballMotion.dy;
-
+#pragma endregion
+#pragma region render
 		SDL_SetRenderDrawColor(renderer, 26, 16, 46, 255);
 		SDL_RenderClear(renderer);
 
@@ -219,30 +231,17 @@ int main(int argc, char** args)
 
 		SDL_SetRenderDrawColor(renderer, 255, 16, 46, 255);
 
-#ifdef ENABLE_BALL_TRACE
-		trace.push_back(ballCenter);
-
-		for (size_t i = 1; i < trace.size(); i++)
-		{
-			Position prevDot = trace.at(i - 1);
-			Position currentDot = trace.at(i);
-
-			SDL_SetRenderDrawColor(renderer, 255, 16, 46, 255);
-			SDL_RenderDrawLine(renderer, prevDot.x, prevDot.y, currentDot.x, currentDot.y);
-		}
-
-		if (trace.size() > 50)
-		{
-			trace.erase(trace.begin());
-		}
-		
-#endif
 		SDL_RenderPresent(renderer);
 	}
-	
+#pragma endregion
+
+#pragma endregion
+
+#pragma region free
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
+#pragma endregion
 
 	return 0;
 }
