09474f03940a394b550da1269acd2c44389f3ec9
Junhyeok Oh <odocodani@gmail.com>
2025-08-14 00:53:14 +0900

    Add bricks

--- Breakout/Breakout.cpp
+++ Breakout/Breakout.cpp
@@ -1,8 +1,9 @@
 #include <SDL2/SDL.h>
 #include <iostream>
-#include <vector>
+#include <algorithm>
 #include <chrono>
 #include <cmath>
+#include <cstdlib>
 
 #pragma region types
 struct Position
@@ -22,6 +23,30 @@ struct Size
 	int width;
 	int height;
 };
+
+struct Color
+{
+	int r;
+	int g;
+	int b;
+};
+
+struct Brick
+{
+	SDL_Rect rect;
+	Color color;
+	bool broken;
+};
+
+enum class CollisionEdge
+{
+	None = 0,
+	Top, 
+	Bottom, 
+	Left, 
+	Right, 
+};
+
 #pragma endregion
 #pragma region functions
 static void DrawCircle(SDL_Renderer* renderer, Position center, int radius)
@@ -56,6 +81,67 @@ static void DrawCircle(SDL_Renderer* renderer, Position center, int radius)
 		}
 	}
 }
+
+static bool IsCircleRectIntersecting(Position circleCenter, int circleRadius, const SDL_Rect& rect)
+{
+	Position closestPoint{};
+	closestPoint.x = std::max((float)rect.x, std::min(circleCenter.x, (float)(rect.x + rect.w)));
+	closestPoint.y = std::max((float)rect.y, std::min(circleCenter.y, (float)(rect.y + rect.h)));
+
+	float dx = circleCenter.x - closestPoint.x;
+	float dy = circleCenter.y - closestPoint.y;
+
+	return (dx * dx + dy * dy) <= (circleRadius * circleRadius);
+}
+
+static CollisionEdge GetCircleRectIntersectionEdge(Position circleCenter, int circleRadius, const SDL_Rect& rect)
+{
+	Position closestPoint{};
+	closestPoint.x = std::max((float)rect.x, std::min(circleCenter.x, (float)rect.x + rect.w));
+	closestPoint.y = std::max((float)rect.y, std::min(circleCenter.y, (float)rect.y + rect.h));
+
+	float dx = circleCenter.x - closestPoint.x;
+	float dy = circleCenter.y - closestPoint.y;
+
+	if (dx * dx + dy * dy > circleRadius * circleRadius)
+	{
+		return CollisionEdge::None;
+	}
+
+	Position rectCenter{};
+	rectCenter.x = rect.x + rect.w / 2.0f;
+	rectCenter.y = rect.y + rect.h / 2.0f;
+
+	float dxCenter = circleCenter.x - rectCenter.x;
+	float dyCenter = circleCenter.y - rectCenter.y;
+
+	float verticalFactor = (rect.w / 2.0f + circleRadius) * dyCenter;
+	float horizontalFactor = (rect.h / 2.0f + circleRadius) * dxCenter;
+
+	if (verticalFactor > horizontalFactor)
+	{
+		if (verticalFactor > -horizontalFactor)
+		{
+			return CollisionEdge::Bottom;
+		}
+		else
+		{
+			return CollisionEdge::Left;
+		}
+	}
+	else
+	{
+		if (verticalFactor > -horizontalFactor)
+		{
+			return CollisionEdge::Right;
+		}
+		else
+		{
+			return CollisionEdge::Top;
+		}
+	}
+}
+
 #pragma endregion
 
 int main(int argc, char** args)
@@ -73,13 +159,13 @@ int main(int argc, char** args)
 	bool running = true;
 
 	// paddle
-	Size paddleSize = { 200, 10 };
+	Size paddleSize = { 150, 10 };
 	Position paddlePos = { (windowSize.width - paddleSize.width) / 2.0f, (float)((windowSize.height - 40) - paddleSize.height)};
 	int paddleVelocity = 15;
 	SDL_Rect paddleShape{};
 
 	// ball
-	int ballRadius = 10;
+	int ballRadius = 8;
 	Position ballCenter = { paddlePos.x + paddleSize.width / 2, paddlePos.y - ballRadius * 2};
 	float ballVelocity = 7;
 	Motion ballMotion = { -ballVelocity, -ballVelocity };
@@ -94,6 +180,35 @@ int main(int argc, char** args)
 	float bounceAngle;
 	float bounceAngleBoundary = (float)M_PI / 18.0f;
 
+	// bricks
+	Color rainbow[10] = {
+		{255, 0, 0},
+		{255, 127, 0},
+		{255, 255, 0},
+		{127, 255, 0},
+		{0, 255, 0},
+		{0, 255, 127},
+		{0, 255, 255},
+		{0, 127, 255},
+		{0, 0, 255},
+		{127, 0, 255}
+	};
+	Brick bricks[200];
+	for (int i = 0; i < sizeof(bricks) / sizeof(Brick); i++)
+	{
+		Brick& brick = bricks[i];
+
+		brick.rect.w = 40;
+		brick.rect.h = 15;
+
+		brick.rect.x = (800 - brick.rect.w * 20) / 2 + brick.rect.w * (i % 20);
+		brick.rect.y = 100 + brick.rect.h * (i / 20);
+
+		int row = i / 20;
+		brick.color = rainbow[row];
+
+		brick.broken = false;
+	}
 #pragma endregion
 #pragma region init
 	// init SDL
@@ -157,14 +272,17 @@ int main(int argc, char** args)
 		// ball bounce (leftwall, rightwall, topwall)
 		if (ballCenter.x - ballRadius < 0)
 		{
+			ballCenter.x = (float)ballRadius;
 			ballMotion.dx *= -1;
 		}
 		if (ballCenter.x + ballRadius > windowSize.width)
 		{
+			ballCenter.x = (float)(windowSize.width - ballRadius);
 			ballMotion.dx *= -1;
 		}
 		if (ballCenter.y - ballRadius < 0)
 		{
+			ballCenter.y = (float)ballRadius;
 			ballMotion.dy *= -1;
 		}
 
@@ -209,6 +327,36 @@ int main(int argc, char** args)
 			
 			ballCenter.y = paddlePos.y - ballRadius - 1;
 		}
+		
+		// ball-brick collision detection
+		for (Brick& brick : bricks)
+		{
+			if (!brick.broken && IsCircleRectIntersecting(ballCenter, ballRadius, brick.rect))
+			{
+				brick.broken = true;
+				
+				switch (GetCircleRectIntersectionEdge(ballCenter, ballRadius, brick.rect))
+				{
+				case CollisionEdge::Top:
+					ballMotion.dy *= -1;
+					break;
+				case CollisionEdge::Bottom:
+					ballMotion.dy *= -1;
+					break;
+				case CollisionEdge::Left:
+					ballMotion.dx *= -1;
+					break;
+				case CollisionEdge::Right:
+					ballMotion.dx *= -1;
+					break;
+				default:
+					__debugbreak();
+					break;
+				}
+
+				break;
+			}
+		}
 
 		// move ball
 		ballCenter.x += ballMotion.dx;
@@ -224,6 +372,15 @@ int main(int argc, char** args)
 
 		SDL_SetRenderDrawColor(renderer, 150, 150, 150, 255);
 		DrawCircle(renderer, ballCenter, ballRadius);
+
+		for (Brick& brick : bricks)
+		{
+			if (!brick.broken)
+			{
+				SDL_SetRenderDrawColor(renderer, brick.color.r, brick.color.g, brick.color.b, 255);
+				SDL_RenderFillRect(renderer, &(brick.rect));
+			}
+		}
 
 		SDL_SetRenderDrawColor(renderer, 255, 16, 46, 255);
 
